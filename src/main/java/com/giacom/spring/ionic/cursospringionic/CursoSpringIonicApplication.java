b67bb70e0b04118a61e1fe2ffbd0615865261d7e
Sandro Giacomozzi <sandrogiacom@gmail.com>
2018-09-02 10:24:01 -0300

    Init commit

---
+++ src/main/java/com/giacom/spring/ionic/cursospringionic/CursoSpringIonicApplication.java
@@ -0,0 +1,12 @@
+package com.giacom.spring.ionic.cursospringionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringIonicApplication.class, args);
+	}
+}
