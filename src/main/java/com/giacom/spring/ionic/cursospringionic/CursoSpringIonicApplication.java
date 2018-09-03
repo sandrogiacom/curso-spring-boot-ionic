ecced7e30a5907df31450a6cb63f5e9818f0686a
Sandro Giacomozzi <sandrogiacom@gmail.com>
2018-09-03 08:28:22 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/giacom/spring/ionic/cursospringionic/CursoSpringIonicApplication.java
+++ src/main/java/com/giacom/spring/ionic/cursospringionic/CursoSpringIonicApplication.java
@@ -13,6 +13,7 @@ import com.giacom.spring.ionic.cursospringionic.domain.Cidade;
 import com.giacom.spring.ionic.cursospringionic.domain.Cliente;
 import com.giacom.spring.ionic.cursospringionic.domain.Endereco;
 import com.giacom.spring.ionic.cursospringionic.domain.Estado;
+import com.giacom.spring.ionic.cursospringionic.domain.ItemPedido;
 import com.giacom.spring.ionic.cursospringionic.domain.Pagamento;
 import com.giacom.spring.ionic.cursospringionic.domain.PagamentoComBoleto;
 import com.giacom.spring.ionic.cursospringionic.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.giacom.spring.ionic.cursospringionic.repositories.CidadeRepository;
 import com.giacom.spring.ionic.cursospringionic.repositories.ClienteRepository;
 import com.giacom.spring.ionic.cursospringionic.repositories.EnderecoRepository;
 import com.giacom.spring.ionic.cursospringionic.repositories.EstadoRepository;
+import com.giacom.spring.ionic.cursospringionic.repositories.ItemPedidoRepository;
 import com.giacom.spring.ionic.cursospringionic.repositories.PagamentoRepository;
 import com.giacom.spring.ionic.cursospringionic.repositories.PedidoRepository;
 import com.giacom.spring.ionic.cursospringionic.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class CursoSpringIonicApplication implements CommandLineRunner {
 
     @Autowired
     private PagamentoRepository pagamentoRepository;
+
+    @Autowired
+    private ItemPedidoRepository itemPedidoRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursoSpringIonicApplication.class, args);
@@ -118,6 +123,19 @@ public class CursoSpringIonicApplication implements CommandLineRunner {
 
         pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
         pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
+        ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+        ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+        ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+        ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+        ped2.getItens().addAll(Arrays.asList(ip3));
+
+        p1.getItens().addAll(Arrays.asList(ip1));
+        p2.getItens().addAll(Arrays.asList(ip3));
+        p3.getItens().addAll(Arrays.asList(ip2));
+
+        itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 
     }
 }
